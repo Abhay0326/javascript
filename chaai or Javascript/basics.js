8867ff84a7f98d649b54e10339dc8e599fd3120e
Abhay <abhi0782004@gmail.com>
2026-02-23 20:02:11 +0530

    FinalChanges in basics.js: String, numbers And Maths class.

--- chaai or Javascript/basics.js
+++ chaai or Javascript/basics.js
@@ -68,13 +68,63 @@ console.log("4">=4)
 
 const name="Abhay" //data type: String
 const repo=4
-let course=String("BCA"); //this will create a primitive String not an object.
-const string=new String(`Javascript ${repo}`) //making a sring-Object using String() function with new keyword.
+let course=String("     BCA    "); //this will create a primitive String not an object.
+const strObj=new String(`Javascript ${repo} repositories.`) //making a sring-Object using String() function with new keyword.
 
 console.log(name,repo+" repos") //concatination
 
 //String Interpolation backTick/graveAccent(``)
 console.log(`My name is ${name}. I have ${repo} repositories.`) //used to create formated string.
 
-console.log(typeof name) 
-console.log(string)
+//---------------------String methods-----------------------
+console.log(strObj.at(-5)) //can take negative index also
+console.log(strObj.charAt(5)) //cannot take negative index.
+console.log(strObj[5]) //return character at positive index only.
+console.log(strObj.charCodeAt(5)) //return unicode integer value.
+
+console.log(strObj.includes("repo")) //check given substring is part of string or not.
+console.log(strObj.indexOf("r")) //return index of substring, -1 if not found.
+console.log(strObj.lastIndexOf("r")) //same as indexOf() but start search from last.
+
+console.log(strObj.endsWith(".")) //true
+console.log(strObj.startsWith("java")) //false
+console.log(strObj.repeat(2)) //return a string contains given number of copies concated togather.
+console.log(strObj.replace("i","I")) //(only once)find the 1st subString and replace it with 2nd subString.
+console.log(strObj.replaceAll("i","I")) //replace all the occurance.
+
+console.log(strObj.substring(-2,10)) //(negative index not valid)1st parameter -> startIndex & 2nd -> number of character to be extract.
+console.log(strObj.slice(2,-5)) //(negative index valid with some caution) Used to slice subString(general rules of slicing followed).
+console.log(strObj.split(" ",2)) //1st parameter -> from where to split and **2nd -> number of elements in List of subStrings.
+console.log(course.trim()) //remove left and right whitespaces.
+
+console.log(strObj.toUpperCase())
+console.log(strObj.toLowerCase())
+
+////////////////////////////////////////////////////////////////////////////
+//----------------------------Number and Maths---------------------------//
+
+const n=200 //dynamic type casting by compiler.
+const number=new Number(100.4576) //object of Number() class. Explicite type casting.
+
+//-----methods of number class(all method returns a primitive string)
+console.log(number.toString())
+console.log(number.toFixed(2)) //fix number of decimal places and rounded-Off.
+console.log(Number(1000000).toLocaleString("en-US")) //convert number into comma seperated string according to IN/US standard.
+console.log(number.toPrecision(4)) //return a number conerted to given precision. 
+console.log(number.toExponential(3)) //convert given number in exponential notation with given number of decimal places.
+console.log(typeof number.valueOf()) //convert Number object to primitive number.
+
+console.log(Number.MAX_VALUE, Number.MIN_VALUE, Number.MAX_SAFE_INTEGER, Number.POSITIVE_INFINITY)
+
+//--------------Math Class--------------
+console.log(Math.PI, Math.E, Math.LN2)
+console.log(Math.round(number))
+console.log(`Absolute: ${Math.abs(-45)} \nFloor Integer: ${Math.floor(4.76)} \nCeil Integer: ${Math.ceil(4.1)}`)
+
+console.log(Math.random()) //return random no. between 0 to 1.
+//random number [1-10]
+console.log(Math.floor((Math.random()*10) +1)) 
+//random number [min-max)
+const min=22
+const max=30
+console.log(Math.floor((Math.random()*(max-min)) +min)) 
