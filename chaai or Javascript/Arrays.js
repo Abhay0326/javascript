bc7edd7fa6d4e11e44c0eb1bcfcb8ee46282544e
Abhay <abhi0782004@gmail.com>
2026-03-28 14:05:39 +0530

    Edit in Arrays.js:Array_2

--- chaai or Javascript/Arrays.js
+++ chaai or Javascript/Arrays.js
@@ -21,4 +21,23 @@ y=array.shift()  //remove element from start and return it.
 // console.log(array.join()) //convert array to string.
 
 s1=array.slice(1,3) //return a copy of the sub-array(#last index not included).
+// console.log(s1,array)
 s2=array.splice(1,3) //edit in original array, and return the sub-array(#Including last index). 
+// console.log(s2,array)
+
+const new_array=ar.concat(array) //concatinate two arrays and return a new array
+console.log(new_array,ar)
+
+/***impotant***/
+const new_array2=[...ar,...array,...ar,..."Abhay"] //adding values using SPREAD-opperator(...).
+console.log(new_array2)
+
+let new_ar=Array.of(ar,array,"abhay"); //add given values in an array as a single element.
+console.log(new_ar)
+
+const nestedArray=[[1,2,3],[4,5,6,[7,8]],9,10] 
+console.log(nestedArray.flat(Infinity)) //convert nested array into single array.
+
+console.log(Array.isArray("A")) //return Flase as "A" is not a array.
+console.log(Array.from("Abhay")) //convert an Array like object to array.Return an empty Array[] if cannot convert.
+
